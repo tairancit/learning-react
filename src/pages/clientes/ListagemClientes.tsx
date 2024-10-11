54606ebc5efd591c65edc94661dfad204f8eea69
Tairan Cittadin <tairancit@gmail.com>
2024-10-11 18:47:49 -0300

    Some code

--- src/pages/clientes/ListagemClientes.tsx
+++ src/pages/clientes/ListagemClientes.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import Button from '@mui/joy/Button';
-import Table from '@mui/joy/Table';
-import Typography from '@mui/joy/Typography';
-import IconButton from '@mui/joy/IconButton';
+import React, { useState } from 'react';
+import { Button, Table, Typography, IconButton, Modal } from '@mui/joy';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { useNavigate } from 'react-router-dom';
@@ -18,21 +15,44 @@ interface Cliente {
 const ListagemClientes: React.FC = () => {
   const navigate = useNavigate();
 
-  const handleNavigate = () => {
+  const navigateCadastro = () => {
     navigate('/clientes/cadastro');
   };
 
-  const cliente: Cliente[] = [
+  const [clientes, setClientes] = useState<Cliente[]>([
     { id: 1, cpfCnpj: '286.431.650-18', nome: 'João da Silva', email: 'joao@example.com', telefone: '(48) 3433-1234' },
     { id: 2, cpfCnpj: '68.458.381/0001-90', nome: 'Empresa de Plástico', email: 'maria@example.com', telefone: '(48) 99915-1254' },
     { id: 3, cpfCnpj: '373.937.810-77', nome: 'Maria Silveira', email: 'carlos@example.com' , telefone: '(48) 3433-5343'},
     { id: 4, cpfCnpj: '26.968.908/0001-87', nome: 'Empresa de Tintas', email: 'ana@example.com', telefone: '(48) 3433-6643'},
-  ];
+  ]);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [clienteIdParaExcluir, setClienteIdParaExcluir] = useState<number | null>(null);
+
+  const abrirModal = (id: number) => {
+    setClienteIdParaExcluir(id);
+    setIsModalOpen(true);
+  };
+
+  const excluir = () => {
+    if (clienteIdParaExcluir !== null) {
+      const novosClientes = clientes.filter(cliente => cliente.id !== clienteIdParaExcluir);
+      setClientes(novosClientes);
+      setIsModalOpen(false);
+    }
+  };
+
+  const fecharModal = () => {
+    setIsModalOpen(false);
+    setClienteIdParaExcluir(null); // Limpa o ID do cliente
+  };
 
   return (
     <div style={{ padding: '20px' }}>
       <Typography level="h2" mb={2}>Clientes</Typography>
-      <Button onClick={handleNavigate}>+ Cliente</Button>
+      <Button onClick={navigateCadastro} style={{ marginBottom: '20px' }}>
+        + Cliente
+      </Button>
       <Table color="primary" borderAxis="both" variant="soft" size="sm" stickyHeader stripe="odd">
         <thead>
           <tr>
@@ -44,20 +64,50 @@ const ListagemClientes: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          {cliente.map((cliente) => (
+          {clientes.map((cliente) => (
             <tr key={cliente.id}>
               <td>{cliente.cpfCnpj}</td>
               <td>{cliente.nome}</td>
               <td>{cliente.email}</td>
               <td>{cliente.telefone}</td>
               <td>
-                <IconButton variant="plain"><DeleteIcon /></IconButton>
-                <IconButton variant="plain"><EditIcon /></IconButton>
+                <IconButton variant="plain" onClick={() => abrirModal(cliente.id)}><DeleteIcon /></IconButton>
+                <IconButton variant="plain" onClick={navigateCadastro}><EditIcon /></IconButton>
                 </td>
             </tr>
           ))}
         </tbody>
       </Table>
+      <div style={{ marginTop: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        Página 1 de 10
+        <a>Próximo</a>
+      </div>
+
+      {/* Modal de confirmação */}
+      {isModalOpen && (
+        <Modal open={isModalOpen} onClose={fecharModal}>
+          <div
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              padding: '20px',
+              backgroundColor: 'white',
+              borderRadius: '8px',
+              width: '300px',
+              textAlign: 'center',
+            }}
+          >
+            <Typography>Confirmação</Typography>
+            <Typography mb={2}>Tem certeza que deseja excluir este cliente?</Typography>
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+              <Button onClick={excluir} color="danger">Sim</Button>
+              <Button onClick={fecharModal} color="neutral">Não</Button>
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
